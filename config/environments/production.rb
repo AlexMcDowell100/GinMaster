55b768b8093144a3ed634b8ac9e452c0468480db
AlexMcDowell100 <amcdowell100@hotmail.com>
2018-07-12 14:02:08 +0100

    added heroku link

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,7 +18,7 @@ user_name: ENV['GMAIL_USERNAME'],
 password: ENV['GMAIL_PASSWORD']
 }
 
-Rails.application.routes.default_url_options[:host] = 'https://young-sea-55118.herokuapp.com'
+Rails.application.routes.default_url_options[:host] = 'https://morning-dawn-84586.herokuapp.com'
 
 config.serve_static_assets = true
 
