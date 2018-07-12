7d8dd85b0aacc6c85c45d125540bb1273511cbb3
AlexMcDowell100 <amcdowell100@hotmail.com>
2018-07-12 14:45:21 +0100

    make plan a symbol

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,7 +12,7 @@ config.action_mailer.smtp_settings = {
 address: 'smtp.gmail.com',
 port: 587,
 domain: ENV['GMAIL_DOMAIN'],
-authentication: 'plain',
+authentication: plain,
 enable_starttls_auto: true,
 user_name: ENV['GMAIL_USERNAME'],
 password: ENV['GMAIL_PASSWORD']
